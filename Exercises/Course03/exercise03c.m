98a491f57859e97b05a2eb35726e0bd7473ef208
befriko <befriko@gmail.com>
2023-10-28 13:36:08 +0700

    Minor change in exercise03c
    Minor change in exercise03c to fix source spacing.

--- Exercises/Course03/exercise03c.m
+++ Exercises/Course03/exercise03c.m
@@ -31,7 +31,6 @@ xrec=x(:,1:length(a));zrec=zeros(size(xrec));
 % simply all blanks (no shot exploded), at t=dtstep, the snapshot is blank
 % everywhere except on the shot location.
 snap1=zeros(size(vmodel));
-snap2=snap1;
 
 % create 10 gathers, set zeros matrix for recorded seismogram
 ng=10;seismogram=zeros((tmax/dt+1),length(xrec)*ng);
@@ -41,12 +40,13 @@ b=length(xrec)-1;seis=zeros(size(seismogram));
 tic;
 for i=1:ng
     % where's my shot?
-    xpos=find(x==(i-1)*dx);
+    xshot=find(x==(i-1)*dx);
     % put the explosion on the shot location and then move the receivers
-    snap2(1,xpos)=1;xrec=xrec+((i-1)*dx);
+    snap2=snap1;
+    snap2(1,xshot)=1;xpos=xrec+((i-1)*dx);
     % second order laplacian
     [seismogram(:,i+b*(i-1):i+b*i),seis(:,i+b*(i-1):i+b*i),t]=afd_shotrec(dx,dtstep,dt,tmax,...
-        vmodel,snap1,snap2,xrec,zrec,[5 10 30 40],0,1);
+        vmodel,snap1,snap2,xpos,zrec,[5 10 30 40],0,1);
     fprintf('Finished generating shot gather no. %d \n',i);
 end
 toc;
@@ -54,7 +54,7 @@ toc;
 % display velocity model
 figure;imagesc(x,z,vmodel);
 colormap(jet);colorbar;
-xlabel('Distance (m)');ylabel('Depth (m)');
+xlabel('Distance (ft)');ylabel('Depth (ft)');
 title('Velocity model');
 % display shot records
 %plotimage(seismogram,t,1:length(xrec)*ng);
